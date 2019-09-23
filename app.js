e55cd2f4416000fa4986d24f872775f38cf70e64
Praveen Pal <praveenpal4232@gmail.com>
2019-09-23 21:47:52 +0530

    added app.js and style.css

---
+++ app.js
@@ -0,0 +1,28 @@
+var my_data = [
+    {
+      data_key:"bio",
+      show_data:"Hello World, Bio",
+    },
+    {
+      data_key:"social",
+      show_data:"Hello World, Social Data",
+    },
+    ];
+
+    function run_task(){
+      console.log("Your camand is runing");
+    }
+    var kik = document.getElementsByClassName("get_data");
+    
+    kik[kik.length-1].addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+
+   if(my_data.some(command => command.data_key === kik[kik.length-1].value)){
+    event.preventDefault();
+    console.log("Your Command is running..."+kik[kik.length-1].value);
+   }
+   else{
+    console.log("Your Command is not running..."+kik[kik.length-1].value);
+   }
+}
+});
