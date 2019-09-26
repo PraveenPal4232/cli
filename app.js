5696c4e06bed88fe86d8a9f83972462fe3a22fc8
Praveen Pal <praveenpal4232@gmail.com>
2019-09-26 22:57:29 +0530

    update

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var my_data = [
         "With or without religion, you would have good people doing good things and evil people doing evil things. But for good people to do evil things, that takes religion.",
         "Positive thinking will let you do everything better than negative thinking will.",
         "You have enemies? Good. That means you’ve stood up for something, sometime in your life.",
-        "You've been living in a dream world, Neo.",
+        "Once you replace negative thoughts with positive ones, you’ll start having positive results.",
         "What is \"real\"? How do you define \"real\"?",
         "Welcome to the real world.",
         "Because you have been down there Neo, you know that road, you know exactly where it ends. And I know that's not where you want to be.",
