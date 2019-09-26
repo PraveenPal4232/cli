e32c74ede646f5f76aed62827a4e883b38f78227
Praveen Pal <praveenpal4232@gmail.com>
2019-09-26 23:04:10 +0530

    update

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ var my_data = [
         "A man must be big enough to admit his mistakes, smart enough to profit from them and strong enough to correct them.",
         "A man must be big enough to admit his mistakes, smart enough to profit from them and strong enough to correct them.",
         "A man must be big enough to admit his mistakes, smart enough to profit from them and strong enough to correct them.",
+        "A man must be big enough to admit his mistakes, smart enough to profit from them and strong enough to correct them.",
       ],
     },
     {
