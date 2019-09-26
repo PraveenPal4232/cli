0e93c2551c4cad30434816f35988a03c0bedfa30
Praveen Pal <praveenpal4232@gmail.com>
2019-09-26 22:57:06 +0530

    update

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var my_data = [
         "Kindness is the language which the deaf can hear and the blind can see.",
         "With or without religion, you would have good people doing good things and evil people doing evil things. But for good people to do evil things, that takes religion.",
         "Positive thinking will let you do everything better than negative thinking will.",
-        "Choi: \"It just sounds to me like you need to unplug, man.",
+        "You have enemies? Good. That means you’ve stood up for something, sometime in your life.",
         "You've been living in a dream world, Neo.",
         "What is \"real\"? How do you define \"real\"?",
         "Welcome to the real world.",
