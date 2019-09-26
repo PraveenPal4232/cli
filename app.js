dc48bc198163de9f65ff2ac4433ddf8f99d598e0
Praveen Pal <praveenpal4232@gmail.com>
2019-09-26 23:03:40 +0530

    update

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var my_data = [
         "If the only prayer you ever say in your entire life is ‘Thank you!’, it will be enough.",
         "A man must be big enough to admit his mistakes, smart enough to profit from them and strong enough to correct them.",
         "A man must be big enough to admit his mistakes, smart enough to profit from them and strong enough to correct them.",
+        "A man must be big enough to admit his mistakes, smart enough to profit from them and strong enough to correct them.",
       ],
     },
     {
