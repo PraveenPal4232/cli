e0729d33edf12bb7f5e5a440bc52341aadcf771c
Praveen Pal <praveenpal4232@gmail.com>
2019-09-26 22:56:42 +0530

    update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var my_data = [
         "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.",
         "Kindness is the language which the deaf can hear and the blind can see.",
         "With or without religion, you would have good people doing good things and evil people doing evil things. But for good people to do evil things, that takes religion.",
-        "The time has come to make a choice.",
+        "Positive thinking will let you do everything better than negative thinking will.",
         "Choi: \"It just sounds to me like you need to unplug, man.",
         "You've been living in a dream world, Neo.",
         "What is \"real\"? How do you define \"real\"?",
