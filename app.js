d9301efd69da13daf2aafcf95d1432f2f74b36c5
Praveen Pal <praveenpal4232@gmail.com>
2019-10-02 09:18:23 +0530

    update

--- app.js
+++ app.js
@@ -66,9 +66,12 @@ var my_data = [
     var input = document.getElementById("playground").lastChild;
     console.log(input);
 
-// Execute a function when the user releases a key on the keyboard
 input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
+    // Cancel the default action, if needed
     event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
   }
 });
