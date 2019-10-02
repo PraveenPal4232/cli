0b1cea9134361b3268f7be805cfa2d525653e4ff
Praveen Pal <praveenpal4232@gmail.com>
2019-10-02 09:19:46 +0530

    update

--- app.js
+++ app.js
@@ -67,11 +67,8 @@ var my_data = [
     console.log(input);
 
 input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     document.getElementById("myBtn").click();
   }
 });
